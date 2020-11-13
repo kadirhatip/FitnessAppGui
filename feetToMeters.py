3fd41dd87e1519d972ebd8d9a1f931b5c2d13f68
Kadir Hatip <59515899+kadirhatip@users.noreply.github.com>
2020-11-13 18:15:24 +0100

    Finished

--- feetToMeters.py
+++ feetToMeters.py
@@ -1,12 +1,45 @@
 # gutes Tutorial: https://tkdocs.com/tutorial/firstexample.html
 
-
+# Importing necessary modules
 from tkinter import *
 from tkinter import ttk
 
+def calculate(*args):
+    try:
+        value = float(feet.get())
+        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
+    except ValueError:
+        pass
+
+# Setting up the main application Window
 root = Tk()
-root.title("Feet to Meters")
+root.title("Feet to meter")
+
+# Creating a content frame
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)	
+root.rowconfigure(0, weight=1)
+
+# Creating the Widgets
+feet = StringVar()
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=(W, E))
+
+meters = StringVar()
+ttk.Label(mainframe, textvariable= meters).grid(column=2, row=2, sticky=(W, E))
+
+ttk.Button(mainframe, text="Calculate", command= calculate).grid(column=3, row=3, sticky=W)
+
+ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="is equal to").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+
+for child in mainframe.winfo_children():
+    child.grid_configure(padx= 5, pady= 5)
+
+feet_entry.focus()
+root.bind("<Return>", calculate)
+
+
+root.mainloop()
